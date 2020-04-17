e96a2fe9634e0ded64ed2f2b48ca09eb25243cfc
xdoo <claus.straube@gmail.com>
2020-04-17 08:49:02 +0200

    fd component

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,10 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="black"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <span class="headline">FileDrop</span><span class="font-weight-light headline">Sample</span>
 
       <v-spacer></v-spacer>
 
